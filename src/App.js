9b43b1bffb68614c0770b28735c988cb9aeace1e
murrayadrian <vinhduongtrung812@outlook.com>
2023-06-17 10:38:31 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import  Axios  from "axios";
+import { useEffect } from "react";
 function App() {
+  useEffect(()=>{
+    Axios.get("http://localhost:8888/product/getAll")
+    .then(res => console.log(res.data)).catch(err => console.log(err))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      ok
     </div>
   );
 }
